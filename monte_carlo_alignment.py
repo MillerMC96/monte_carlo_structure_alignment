c95b1f4f36b8899db095edfbb5643efacb940fc6
MillerMC96 <pywang@ucdavis.edu>
2020-12-16 14:56:19 -0800

    init

---
+++ monte_carlo_alignment.py
@@ -0,0 +1,9 @@
+################################################################################
+# python script for structure alignment using Monte Carlo method               #
+# Author: Panyue Wang                                                          #
+# Email: pywang@ucdavis.edu                                                    #
+################################################################################
+import numpy as np
+
+if __name__ == "__main__":
+    pass
