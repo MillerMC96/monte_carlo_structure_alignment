896e6ae118f15dc9e0fb04b3e7d843bb188f7b4e
MillerMC96 <pywang@ucdavis.edu>
2020-12-16 20:32:13 -0800

    added all coords

--- monte_carlo_alignment.py
+++ monte_carlo_alignment.py
@@ -10,17 +10,21 @@ import sys
 def pdb_parser(pdb_file_obj):
     xyz = np.zero(3)
     ca_coords = []
+    atom_coords = []
     for line in pdb_file_obj:
         line_arr = line.split()
-        # only get C-alphas
+        xyz[0] = line_arr[6]
+        xyz[1] = line_arr[7]
+        xyz[2] = line_arr[8]
+        # get C-alphas
         if line_arr[2] == "CA":
-            xyz[0] = line_arr[6]
-            xyz[1] = line_arr[7]
-            xyz[2] = line_arr[8]
             ca_coords.append(xyz)
+        atom_coords.append(xyz)
     
-    return np.array(ca_coords)
+    return np.array(ca_coords), np.array(atom_coords)
 
 if __name__ == "__main__":
     input_pdb = open(sys.argv[1], 'r')
+    ca, atom = pdb_parser
+
     pass
