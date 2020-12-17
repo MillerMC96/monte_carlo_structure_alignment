ce9042a8cfcf74a21d1176552fa51a31b04fcb66
MillerMC96 <pywang@ucdavis.edu>
2020-12-16 20:19:36 -0800

    added parser

--- monte_carlo_alignment.py
+++ monte_carlo_alignment.py
@@ -4,6 +4,21 @@
 # Email: pywang@ucdavis.edu                                                    #
 ################################################################################
 import numpy as np
+import sys
+
+def pdb_parser(pdb_file_obj):
+    xyz = np.zero(3)
+    ca_coords = []
+    for line in pdb_file_obj:
+        line_arr = line.split()
+        # only get C-alphas
+        if line_arr[2] == "CA":
+            xyz[0] = line_arr[6]
+            xyz[1] = line_arr[7]
+            xyz[2] = line_arr[8]
+            ca_coords.append(xyz)
+    
+    return np.array(ca_coords)
 
 if __name__ == "__main__":
     pass
